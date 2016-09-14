a0797e435779fee84f6e05af09452278e80954e9
almookie <al_mookie@hotmail.com>
2016-09-14 22:05:48 +1000

    Removed Jackie's Test Code
    Removed the test code to see if Jackie's repository was working, also
    added my own changes to Subject.java

--- src/Subject.java
+++ src/Subject.java
@@ -1,4 +1,41 @@
 
 public class Subject {
+	private String name;
+	private String shortcode;
+	
+	public Subject(String name, String shortcode) {
+		this.name = name;
+		this.shortcode = shortcode;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName(String name) {
+		return name;
+	}
+	
+	public void setShortcode(String shortcode) {
+		this.shortcode = shortcode;
+	}
+	
+	public String getShortcode(String shortcode) {
+		return shortcode;
+	}
+	
+	@Override
+	public boolean equals (Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Subject)) return false;
+		Subject subject = (Subject) o;
+		return name.equals(subject.name);
+	}
+		
+	@Override
+	public int hashCode() {
+		final int result = 17;
+		return 37 * result + name.hashCode();
+	}	
 
 }
