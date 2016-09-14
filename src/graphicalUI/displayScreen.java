531aea7978f1382ad9fe44eeb2c66d175c6a9fee
leonaugustine <Leon@MacBook-Pro-4.local>
2016-09-14 23:22:55 +1000

    graphical module with main, presumably calling/instantiating backend from here

---
+++ src/graphicalUI/displayScreen.java
@@ -0,0 +1,34 @@
+package graphicalUI;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class displayScreen {
+	
+    private static void createAndShowGUI() {
+    	 final JFrame frame = new JFrame();
+         frame.setTitle("Test Frame");
+         
+         //Note: This gives the effect of the page expanding 
+         //from the center of the page
+         
+         //Center the display
+         frame.setLocationRelativeTo(null);
+         
+         //maximise to display size
+         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
