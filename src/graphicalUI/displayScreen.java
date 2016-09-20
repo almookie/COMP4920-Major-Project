4afb1b38fece51ed4079c5604a6df745afd3e5dd
leonaugustine <Leon@MacBook-Pro-4.local>
2016-09-20 21:37:12 +1000

    menu buttons indiudal

--- src/graphicalUI/displayScreen.java
+++ src/graphicalUI/displayScreen.java
@@ -7,8 +7,18 @@ public class displayScreen {
 	
     private static void createAndShowGUI() {
     	 final JFrame frame = new JFrame();
-         frame.setTitle("Test Frame");
-         
+    	 
+    	 
+    	 //sample buttons for menue using box layout verticl span
+    	 Panel menu = new Panel();
+    	 menu.add(new Button("A"));
+    	 menu.add(new Button("B"));
+    	 menu.add(new Button("C"));
+    	 menu.add(new Button("D"));
+    	 menu.add(new Button("E"));
+    	 menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
+
+
          //Note: This gives the effect of the page expanding 
          //from the center of the page
          
@@ -16,13 +26,17 @@ public class displayScreen {
          frame.setLocationRelativeTo(null);
 
          frame.setLayout(new BorderLayout());
-         frame.add(new Button("Menue"),   BorderLayout.WEST);
+          
+         //two panes, menu and mane pane
+         frame.add(menu,   BorderLayout.WEST);
          frame.add(new Button("PAne"), BorderLayout.CENTER);
          //maximise to display size
          frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          frame.setVisible(true);
+         frame.setResizable(false);
+
     }
 
     public static void main(String[] args) {
