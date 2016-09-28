3796e0df96f6caf9de48b693d51e36d9889061ce
ExaQuaTrZ <ExaQuaTrZ>
2016-09-28 21:41:40 +1000

    ui: added buttons to filter bar

--- src/graphicalUI/displayScreen.java
+++ src/graphicalUI/displayScreen.java
@@ -83,7 +83,7 @@ public class displayScreen {
 
     }
 
-/*    public static void main(String[] args) {
+    public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -91,5 +91,5 @@ public class displayScreen {
                 createAndShowGUI();
             }
         });
-    }*/
+    }
 }
